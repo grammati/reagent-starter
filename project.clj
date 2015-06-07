b099b5fa6962676b976b8174039665ac82471f2a
Chris Perkins <cperkins@rallydev.com>
2015-06-07 08:41:06 -0600

    Simplified

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject reagent-starter "0.1.0-SNAPSHOT"
+  
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license "MIT License"
@@ -6,45 +7,36 @@
   :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3196" :scope "provided"]
-                 [cljsjs/react "0.13.1-0"]
-                 [reagent "0.5.0" :exclusions [cljsjs/react]]
-                 [environ "1.0.0"]]
+                 [org.clojure/clojurescript "0.0-3211"]
+                 [reagent "0.5.0"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-environ "1.0.0"]]
-
-  :min-lein-version "2.5.0"
+            [lein-figwheel "0.3.1"]]
 
   :clean-targets ^{:protect false} ["resources/public/js"]
 
-  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
-                             :compiler {:output-to     "resources/public/js/app.js"
-                                        :output-dir    "resources/public/js/out"
-                                        :asset-path   "js/out"
-                                        :optimizations :none
-                                        :pretty-print  true
-                                        :main          "reagent-starter.core"
-                                        }}}}
+  :profiles {:dev {:source-paths ["dev/clj"]}}
 
-  :profiles {:dev {:repl-options {:init-ns reagent-starter.repl
-                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :cljsbuild {:builds
+              {:dev  {:source-paths ["src/cljs" "dev/cljs"]
+                      :compiler {:output-to     "resources/public/js/dev.js"
+                                 :output-dir    "resources/public/js/out"
+                                 :asset-path    "js/out"
+                                 :optimizations :none
+                                 :pretty-print  true
+                                 :source-map    true
+                                 :main          "reagent-starter.dev"
+                                 }}
+               :prod {:source-paths ["src/cljs" "prod/cljs"]
+                      :compiler {:output-to     "resources/public/js/prod.js"
+                                 :optimizations :advanced
+                                 :pretty-print  false
+                                 :main          "reagent-starter.prod"
+                                 }}}}
 
-                   :dependencies [[figwheel "0.2.6"]]
+  :figwheel {:http-server-root "public"
+             :server-port      3449
+             :nrepl-port       7888
+             :css-dirs         ["resources/public/css"]}
 
-                   :source-paths ["env/dev/clj"]
-                   :plugins [[lein-figwheel "0.2.6"]]
-
-                   :injections [(require 'pjstadig.humane-test-output)
-                                (pjstadig.humane-test-output/activate!)]
-
-                   :figwheel {:http-server-root "public"
-                              :server-port 3449
-                              :nrepl-port  7888
-                              :css-dirs ["resources/public/css"]}
-
-                   :env {:dev? true}
-
-                   :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]
-                                              :compiler {:main "reagent-starter.dev"
-                                                         :source-map true}}}}}})
+  ) 
